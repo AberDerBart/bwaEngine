c87ddee074f9e64aea8fd87f208899ba2d02a770
AberDerBart <nonatz@web.de>
2019-05-08 14:08:36 +0200

    load_map.py: add animation

--- tools/load_map.py
+++ tools/load_map.py
@@ -13,11 +13,16 @@ args = parser.parse_args()
 screen = pygame.display.set_mode((640,480))
 
 gameMap = junebugEngine.MapParser.parse(args.map)
+viewport = junebugEngine.Viewport((640,480), gameMap)
 
-gameMap.render(screen,(0,0))
-pygame.display.update()
+clock = pygame.time.Clock()
 
 while(True):
+	
+	viewport.update(clock.tick())
+	screen.blit(viewport.surf, (0,0))
+	pygame.display.update()
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit(0)
